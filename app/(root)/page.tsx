1732988f66ad96ac168561cb35a386f2b1e191f5
Jose E Rodriguez-Marrero <jose.rodriguez-marrero@proton.me>
2024-05-09 21:28:09 -0700

    Render headerbox with greeting

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Abdul'}
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName}
+            subtext="Access and manage your accounts"
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
